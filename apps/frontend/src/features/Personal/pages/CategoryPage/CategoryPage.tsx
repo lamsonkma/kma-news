5e7d9fbce3f52b2136001dd0afa360c841ee1dee
Nguyễn Lam Sơn <67233512+lamsonkma@users.noreply.github.com>
2022-02-15 22:27:33 -0800

    update (#20)

--- apps/frontend/src/features/Personal/pages/CategoryPage/CategoryPage.tsx
+++ apps/frontend/src/features/Personal/pages/CategoryPage/CategoryPage.tsx
@@ -4,24 +4,55 @@ import {
   selectChannel,
   getPersonalChannelAction,
 } from '@kma-news/channel-slice';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { GiTrashCan } from 'react-icons/gi';
+import { HiOutlineDotsHorizontal, HiOutlinePencilAlt } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 function CategoryPageMain() {
   const channels = useAppSelector(selectChannel);
   const dispatch = useAppDispatch();
+  const [dropItem, setDropItem] = useState(false);
   useEffect(() => {
     dispatch(getPersonalChannelAction());
   }, [dispatch]);
   if (channels.length > 0)
     return (
-      <>
-        <div>Danh sách: </div>
-        <div>
-          {channels.map((e) => (
-            <div>{e.name}</div>
+      <div className="category-page-main">
+        <div className="header-category-page-main">
+          <h3>Tin tức tùy chọn</h3>
+          <Link className="btn create-category" to={'tao-moi'}>
+            Tạo mục
+          </Link>
+        </div>
+        <div className="list-category-page-main col-12">
+          {channels.map((e, i) => (
+            <div className="item-category-page-main">
+              <div className="item-img">
+                <img
+                  src="https://photo-baomoi.zadn.vn/w300_r3x2_sm/2022_02_16_119_41775537/da0037feadbc44e21dad.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="item-name">{e.name}</div>
+              <div className="btn-item" onClick={() => setDropItem(!dropItem)}>
+                <HiOutlineDotsHorizontal />
+                {dropItem && (
+                  <div className="item-category-drop">
+                    <div className="update-category">
+                      <HiOutlinePencilAlt size="20px" />
+                      <span>Sửa</span>
+                    </div>
+                    <div className="remove-category">
+                      <GiTrashCan size="20px" />
+                      <span>Xóa</span>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
           ))}
         </div>
-      </>
+      </div>
     );
   return (
     <div className="category-page-main">
